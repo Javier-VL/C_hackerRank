b2735e8f3f055707122e0b4c74bec66bb4e90184
Javier-Vl <jvelazquez.lozano@iteso.mx>
2022-03-22 21:32:58 -0600

    primer ejercicio

---
+++ 01-hello.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) <year> <name of author>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+
+// https://www.hackerrank.com/challenges/hello-world-c/problem?isFullScreen=true
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	char s [100];
+	scanf("%[^\n]%*c", s);
+    printf("Hello, World!\n");
+    printf("%s\n",s);
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+
+
+	return 0;
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 noexpandtab:
+ * :indentSize=8:tabSize=8:noTabs=false:
+ */
+
